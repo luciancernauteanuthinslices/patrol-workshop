84fe77dfaef2819554c5a6ef8c28a4b4ac842a66
Mateusz Wojtczak <mateusz.wojtczak@leancode.pl>
2024-12-03 15:45:48 +0100

    Change bundle id

--- android/app/src/main/kotlin/pl/leancode/patrol/challenge/MainActivity.kt
+++ android/app/src/main/kotlin/pl/leancode/patrol/challenge/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.patrol_challenge
+package com.example.patrol.challenge
 
 import io.flutter.embedding.android.FlutterActivity
 
--- integration_test/quiz_test.dart
+++ integration_test/quiz_test.dart
@@ -1,6 +1,10 @@
 import 'package:patrol/patrol.dart';
 import 'package:patrol_challenge/main.dart';
+import 'package:patrol_challenge/ui/style/colors.dart';
 
+/// Welcome to our Patrol quiz app :)
+/// We're going to test our app with Patrol, a tool for E2E testing Flutter apps.
+/// See [PTColors] for color constants
 void main() {
   patrolTest(
     'test',
